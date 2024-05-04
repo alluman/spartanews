9deae7d4d7561db85cfa380c7a5afb9a8e9437ff
alluman <mathghost11@gmail.com>
2024-05-05 01:18:59 +0900

    login, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name='accounts'
 urlpatterns = [
     path('create/', views.CreateView.as_view(), name='create'),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class CreateView(APIView):
 
@@ -38,4 +39,46 @@ class CreateView(APIView):
             return Response({'message': '저장되었습니다', "userId": user.id}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=400)
+
+class LogoutView(APIView):
+    def post(self, request):
+        token = RefreshToken(request.data.get('refresh'))
+        token.blacklist()
+        return Response({'message': '성공적으로 로그아웃 되었습니다.'}, status.HTTP_200_OK)
+
+
+class PasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    def put(self, request):
+        user = request.user
+        old_password = request.data['old_password']
+        new_password = request.data['new_password']
+        if not user.check_password(old_password):
+            return Response({'message': '예전 password와 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        if new_password == old_password:
+            return Response({'message': '동일한 password입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        user.set_password(new_password)
+        user.save()
+        return Response({'message': '변경되었습니다.'}, status=status.HTTP_200_OK)
         
+
+class ProfileView(APIView):
+    pass
+    # 이름, 가입일, 작성글, 댓글, 좋아요목록, 댓글목록, 찜한뉴스목록, 찜한뉴스댓글목록..이 각각 들어가야함
+    # permission_classes = [IsAuthenticated]
+    # def get(self, request, username):
+    #     user = get_object_or_404(User, username=username)
+    #     serializer = UserUpdateSerializer(user)
+    #     return Response(serializer.data) 
+
+
+    
+class DeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    def delete(self, request):
+        user = request.user
+        password = request.data['password']
+        if not user.check_password(password):
+            return Response({'message': 'password가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        user.delete()
+        return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
